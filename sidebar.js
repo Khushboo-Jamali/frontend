43cf35c989d33de7560e402af85e70ec57cb398f
khushboo-jamali <jamalikhushboo@gmail.com>
2025-01-17 19:56:33 +0500

    today code is that for my website

---
+++ sidebar.js
@@ -0,0 +1,11 @@
+let nav = (document.querySelector("div").innerHTML = `    <div class="sidebar">
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/intro.html">My Intro</a></li>
+            <li><a href="/services.html">Services</a></li>
+            <li><a href="/blog.html">Blog</a></li>
+            <li><a href="/contact.html">Contact</a></li>
+          </ul>
+        </nav>
+      </div> `);
